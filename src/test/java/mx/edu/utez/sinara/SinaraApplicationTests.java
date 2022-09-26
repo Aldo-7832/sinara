d3235e5c61be0c248649fe793f86158660b9f646
Aldo Hernández <aldohernandez@utez.edu.mx>
2022-09-25 23:15:24 -0500

    initial commit

---
+++ src/test/java/mx/edu/utez/sinara/SinaraApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.sinara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
